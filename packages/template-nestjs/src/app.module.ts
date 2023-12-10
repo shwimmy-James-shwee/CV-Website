09d745d2cf87acd97774b7f88384bde15b5663d7
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-12-11 10:21:29 +1300

    feat: bulk update pkgs, enable global eslint rules for all pkgs, fixed lint issues

--- packages/template-nestjs/src/app.module.ts
+++ packages/template-nestjs/src/app.module.ts
@@ -1,9 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from '@/modules/user.module';
 import { LoggerMiddleware } from '@/middlewares/logger.middleware';
 import { UserController } from '@/controllers/user.controller';
