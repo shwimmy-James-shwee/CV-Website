b3e24d02fa6e9827c5609d49fbe25aa3e20d12cd
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-12-20 17:27:47 +1300

    feat: bulk update template: mongo docker template, azure-function w typegraphql, new folder structure for nextjs app

---
+++ packages/template-azure-functions-graphql/src/repositories/account.repository.ts
@@ -0,0 +1,20 @@
+import { Service } from 'typedi';
+
+@Service()
+export class AccountRepository {
+  public async findOne() {
+    return;
+  }
+
+  public async findMany() {
+    return;
+  }
+
+  public async create() {
+    return;
+  }
+
+  public async update() {
+    return;
+  }
+}
---
+++ packages/template-azure-functions-graphql/src/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Service } from 'typedi';
+
+@Service()
+export class UserService {
+  public async findOne() {
+    return;
+  }
+
+  public async findMany() {
+    return;
+  }
+
+  public async create() {
+    return;
+  }
+
+  public async update() {
+    return;
+  }
+}
