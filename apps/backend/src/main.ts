15e63e64acfcc01eee5c489900eae26a61ecceb1
Mingyang Li <mingyangli118@gmail.com>
2024-08-29 13:46:08 +1200

    feat: make dummy endpoint return proper json for testing utils pkg

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/modules/app.module";
-import * as utils from "@core/utils";
 
 async function bootstrap() {
-  console.log(
-    `Testing usage of function from @core/utils in this app: ${utils.arrayIsEmpty.name}([1]) -> ${utils.arrayIsEmpty([1])}`
-  );
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APP_PORT || 8080);
 }
