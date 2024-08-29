674698434cbe6c94d17ec56c6acd8c4e4273e24f
Mingyang Li <mingyangli118@gmail.com>
2024-08-29 13:26:10 +1200

    feat: tested usage of using @core/utils in all apps, confirm they work locally

--- apps/functions/src/handlers/handler-1.ts
+++ apps/functions/src/handlers/handler-1.ts
@@ -1,10 +1,13 @@
 import { app, HttpHandler, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import * as utils from "@core/utils";
 
 const handler: HttpHandler = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> => {
   const message = `Http function processed the following request:  ${request?.method} ${request?.url}`;
   context.log(message);
 
-  const body: string = JSON.stringify({ message });
+  const utilsPackageTestMessage = `Testing usage of function from @core/utils in this app: ${utils.arrayIsEmpty.name}([1]) -> ${utils.arrayIsEmpty([1])}`
+
+  const body: string = JSON.stringify({ message, utilsPackageTestMessage });
 
   return { status: 200, body };
 };
