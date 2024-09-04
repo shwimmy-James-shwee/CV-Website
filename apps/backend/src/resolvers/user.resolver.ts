7fb0699dfe85f4aa73532d7492d87a527a03c0fc
Mingyang Li <mingyangli118@gmail.com>
2024-09-04 14:22:01 +1200

    feat: init all models, generated updated gql schema

--- apps/backend/src/resolvers/user.resolver.ts
+++ apps/backend/src/resolvers/user.resolver.ts
@@ -1,47 +1,6 @@
-import { Resolver, Query, InputType, Field, Int, ObjectType, Args } from '@nestjs/graphql';
-import { StringFilter } from '@/types/common/string-filter';
-
-@InputType()
-export class UserWhereInput {
-  @Field(() => StringFilter, { nullable: true })
-  id?: StringFilter;
-
-  @Field(() => StringFilter, { nullable: true })
-  firstName?: StringFilter;
-
-  @Field(() => StringFilter, { nullable: true })
-  lastName?: StringFilter;
-}
-
-@InputType()
-export class UserFindManyArgs {
-  @Field(() => Int, { nullable: true })
-  take?: number;
-
-  @Field(() => Int, { nullable: true })
-  skip?: number;
-
-  @Field(() => UserWhereInput, { nullable: true })
-  where?: UserWhereInput;
-}
-
-@ObjectType()
-export class User {
-  @Field(() => String, { nullable: true })
-  id?: string;
-
-  @Field(() => String, { nullable: true })
-  firstName?: string;
-
-  @Field(() => String, { nullable: true })
-  lastName?: string;
-}
-
-@ObjectType()
-export class UserFindManyResponse {
-  @Field(() => [User])
-  items?: User[];
-}
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { UserFindManyResponse } from '@/types/user/user-find-many.response';
+import { UserFindManyArgs } from '@/types/user/user-find-many.args';
 
 @Resolver()
 export class UserResolver {
