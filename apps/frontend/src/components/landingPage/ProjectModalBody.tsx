d7f9230208185251f568bb55636c6c19f3206e34
jamespearce1_kpmg <jamespearce1@kpmg.co.nz>
2024-10-02 14:53:33 +1300

    feat: continue with project modal

--- apps/frontend/src/components/landingPage/ProjectModalBody.tsx
+++ apps/frontend/src/components/landingPage/ProjectModalBody.tsx
@@ -1,4 +1,7 @@
+import { Paper, MobileStepper, Button } from '@mui/material';
+import { Box, useTheme } from '@mui/system';
 import { testProjectDataType } from './ProjectCards';
+import { useState } from 'react';
 
 type ProjectModalBodyProps = {
   projectData: testProjectDataType;
@@ -6,15 +9,59 @@ type ProjectModalBodyProps = {
 
 // const ProjectModalBody = ({ id, title, description, image, highlighted }: testProjectDataType) => {
 const ProjectModalBody = ({ projectData }: ProjectModalBodyProps) => {
+  const theme = useTheme();
+  const [activeImage, setActiveImage] = useState(0);
+  const maxImages = projectData.image.length;
+
+  const handleNext = () => {
+    setActiveImage((prevActiveImage) => prevActiveImage + 1);
+  };
+
+  const handleBack = () => {
+    setActiveImage((prevActiveImage) => prevActiveImage - 1);
+  };
   return (
-    <div className='project-modal-body'>
-      <div>
-        {/* {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`Project image ${index + 1}`} />
-          </div>
-        ))} */}
-      </div>
+    <div>
+      <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+        <Paper
+          square
+          elevation={0}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            height: 50,
+            pl: 2,
+            bgcolor: 'background.default',
+          }}
+        >
+          <img
+            width={'100%'}
+            src={projectData.image[activeImage]}
+            srcSet={projectData.image[activeImage]}
+            alt={`Project image - Image ${activeImage + 1}`}
+            loading='lazy'
+          />
+        </Paper>
+        <Box sx={{ height: 255, maxWidth: 400, width: '100%', p: 2 }}>this could be a description of the image</Box>
+        <MobileStepper
+          variant='text'
+          steps={maxImages}
+          position='static'
+          activeStep={activeImage}
+          nextButton={
+            <Button size='small' onClick={handleNext} disabled={activeImage === maxImages - 1}>
+              Next
+              {theme.direction === 'rtl' ? '<' : '>'}
+            </Button>
+          }
+          backButton={
+            <Button size='small' onClick={handleBack} disabled={activeImage === 0}>
+              {theme.direction === 'rtl' ? '>' : '<'}
+              Back
+            </Button>
+          }
+        />
+      </Box>
       <h2 className='project-title'>{projectData.title}</h2>
       <div className='project-subtitle-dates'>
         <h3 className='project-subtitle'>{'asdasd'}</h3>
